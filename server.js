54c1c27d169e1b523020a7e7b4dacf6bf2efcd46
Finn Chau <finnchau1@gmail.com>
2025-02-10 21:36:32 -0500

    Add assignment declaration and student information

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+/*********************************************************************************
+ * WEB700 – Assignment 03
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+ * of this assignment has been copied manually or electronically from any other source
+ * (including 3rd party web sites) or distributed to other students.
+ *
+ * Name: Dinh Khoa Chau Student ID: 174120238 Date: 2025-02-10
+ *
+ ********************************************************************************/
+
 const express = require('express')
 const path = require('path')
 const collegeData = require('./modules/collegeData')
